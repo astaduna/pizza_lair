09752d2c283fddab1aec1d841d4f684560541d5a
astaduna <112621688+astaduna@users.noreply.github.com>
2023-05-04 11:51:27 +0000

    added files

--- pizza/models.py
+++ pizza/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class PizzaType(models.Model):
+    name = models.CharField(max_length=255)
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=255)
+    descriptions = models.CharField(max_length=255, blank=True)
+    type = models.ForeignKey(PizzaType, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+class PizzaImage(models.Model):
+    image = models.CharField(max_length=999)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
