53e55520111860d29505dc94b8227be7c24c8f80
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2023-05-04 10:59:16 +0000

    fdgd

--- pizza_lair/settings.py
+++ pizza_lair/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'pizza_lair.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pizza-lair-db',
+        'USER': 'pizza-lair-db-user',
+        'PASSWORD': '12345',
+        'HOST': '35.188.85.114',
+        'PORT': '5432'
     }
 }
 
