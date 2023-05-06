ca81a3c99448484bb90aada7a38945a7b5c90f27
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2023-05-06 13:14:31 +0000

    fdghd

--- pizza/models.py
+++ pizza/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class PizzaType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, blank=True)
 
 class Pizza(models.Model):
     name = models.CharField(max_length=255)
