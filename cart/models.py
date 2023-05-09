125c8901c884b0e492448c15eba9bd1abff7fbaa
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2023-05-09 11:06:02 +0000

    hit

--- cart/models.py
+++ cart/models.py
@@ -7,3 +7,7 @@ class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.IntegerField(default=0)
+
+
+def __str__(self):
+    return f'{self.product.name} - {self.quantity} x {self.price}'
