a0d63eaa060aa1911803b4f9ee43c1227965ab25
astaduna <112621688+astaduna@users.noreply.github.com>
2023-05-10 15:09:27 +0000

    w
    w

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
+
 from django.urls import path
 from .import views
 
 urlpatterns = [
     path('', views.index, name='cart-index'),
-
+    path('add_to_cart', views.add_to_cart, name='add_to_cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Cart
 from .models import Product
 
 # Create your views here.
@@ -8,4 +8,11 @@ def index(request):
     context = {'Product': Product.objects.all()}
     return render(request, 'cart/cart.html', context)
 
-
+def add_to_cart(request):
+    product_id = request.GET.get('prod_id')
+    product_name = Product.objects.get(id=product_id)
+    product = Product.objects.filter(id=product_id)
+    for p in product:
+        price = p.price
+        Cart(product=product_name, price=price).save()
+        return redirect('pizza/pizza_menu.html')
