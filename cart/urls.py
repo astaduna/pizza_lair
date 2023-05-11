195a7d4061f4676654f82f07fe304bcd5b62a3b2
astaduna <112621688+astaduna@users.noreply.github.com>
2023-05-11 14:36:13 +0000

    yeeee

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_cart, name='view_cart'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('clear/', views.clear_cart, name='clear_cart'),
 ]
