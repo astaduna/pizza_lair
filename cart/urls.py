95c102ab9f41561ff9f2ff3017ee78cf730fd5ea
astaduna <112621688+astaduna@users.noreply.github.com>
2023-05-11 12:45:56 +0000

    cart BABY

--- cart/urls.py
+++ cart/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='cart-index'),
-    path('cart/', views.view_cart, name='view_cart'),
-    path('cart/add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('cart/clear/', views.clear_cart, name='clear_cart'),
+    path('', views.view_cart, name='view_cart'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('clear/', views.clear_cart, name='clear_cart'),
 ]
