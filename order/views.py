603fb0e028112138bf8ec69280272cb9fbca2b32
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2023-05-11 22:43:39 +0000

    hello

--- order/views.py
+++ order/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from user.models import Profile
 from user.forms.checkout import CheckoutProfileInfo, CheckoutPaymentForm
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from cart.models import Product
+from order.forms.order import OrderForm
+
 
 
 # Create your views here.
@@ -18,7 +23,48 @@ def profile(request):
         'form': CheckoutProfileInfo(instance=profile)
     })
 
+
 def payment(request):
     form = CheckoutPaymentForm()
     return render(request, 'checkout/checkout_2.html',
                   {'form': form})
+
+
+
+@login_required
+def summary(request):
+    profile = Profile.objects.filter(user=request.user).first()
+
+    cart_items = []
+    total_price = 0
+    cart = request.session.get('cart', [])
+    for prod_id in cart:
+        prod = Product.objects.get(id=prod_id)
+        cart_items.append(prod)
+        total_price += int(prod.price)
+
+    context = {
+        'profile': profile,
+        'cart_items': cart_items,
+        'total_price': total_price,
+    }
+
+    return render(request, 'checkout/order_summary.html', context)
+
+
+@login_required
+def create_order(request):
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('confirm-order', pk=post.pk)
+    else:
+        form = OrderForm()
+
+    return render(request, 'checkout/confirm_order.html', {'form': form})
+
+
+
