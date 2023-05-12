baa2d9295ca9f3b29e3fdfce84c5ec4b10dbe48e
gudlaugvest <112634914+gudlaugvest@users.noreply.github.com>
2023-05-12 20:13:04 +0000

    hi

--- order/views.py
+++ order/views.py
@@ -27,14 +27,9 @@ def profile(request):
 def payment(request):
     payment = CheckoutPaymentForm()
     if request.method == 'POST':
-        if payment.is_valid():
-            payment = form.save(commit=False)
-            payment.user = request.user
-            payment.save()
-        return render(redirect('confirm-payed-order'))
-    return render(request, 'checkout/checkout_2.html',
-                  {'form': payment})
-
+        return redirect('confirm-payed-order')
+    else:
+        return render(request, 'checkout/checkout_2.html', {'form': payment})
 
 
 @login_required
@@ -42,44 +37,23 @@ def summary(request):
     profile = Profile.objects.filter(user=request.user).first()
 
     cart_items = []
-    total_price = 0
+    cart_total_price = 0
     cart = request.session.get('cart', {})
-    for prod_id in cart:
+    for prod_id, quantity in cart.items():
         prod = Product.objects.get(id=prod_id)
-        cart_items.append(prod)
-        total_price += int(prod.price)
+        item_total_price = quantity * int(prod.price)
+        cart_total_price += item_total_price
+        cart_items.append({'product': prod, 'quantity': quantity, 'item_total_price': item_total_price})
 
     context = {
         'profile': profile,
         'cart_items': cart_items,
-        'total_price': total_price,
+        'cart_total_price': cart_total_price,
     }
-
-
-@login_required
-def create_order_payed(request):
-    order = Order()
-    order.user = request.user
-    order.is_paid = True
-    order.save()
-    my_products = []
-    print("asdfdsfasdf")
-    print(request.session['cart'])
-    for id in request.session['cart']:
-        print(id)
-        item = Product.objects.filter(pk=id).first()
-        my_products.append(item)
-        order_item = OrderItem()
-        order_item.order = order
-        order_item.product = item
-        order_item.save()
     request.session['cart'] = {}
 
+    return render(request, 'checkout/confirmed.html', context=context)
 
-
-
-
-    return render(request, 'checkout/order_summary.html', {"items": my_products})
 
 def create_order_unpayed(request):
     order = Order()
@@ -92,8 +66,20 @@ def create_order_unpayed(request):
         order_item.order = order
         order_item.product = item
         order_item.save()
-    request.session['cart'] = {}
 
+    return render(request, 'checkout/order_summary.html', {})
+
+def create_order_payed(request):
+    order = Order()
+    order.user = request.user
+    order.is_paid = True
+    order.save()
+    for id in request.session['cart']:
+        item = Product.objects.filter(pk=id).first()
+        order_item = OrderItem()
+        order_item.order = order
+        order_item.product = item
+        order_item.save()
 
 
     return render(request, 'checkout/order_summary.html', {})
--- user/forms/checkout.py
+++ user/forms/checkout.py
@@ -18,12 +18,12 @@ class CheckoutProfileInfo(forms.ModelForm):
 
 class CheckoutPaymentForm(forms.Form):
     name_of_cardholder = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    card_number = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                    'placeholder': '1234-1234-1234-1234',
-                                                                    'pattern': '[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}',
-                                                                    'maxlength': 19,
-                                                                    'title': '1234-1234-1234-1234'}))
-    cvc = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control',
+    credit_card_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                          'placeholder': '1234-1234-1234-1234',
+                                                                          'pattern': '[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}',
+                                                                          'maxlength': 19,
+                                                                          'title': '1234-1234-1234-1234'}))
+    cvc = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',
                                                            'pattern': '[0-9]{3}',
                                                            'maxlength': 3,
                                                            'title': '3 digit number',
@@ -33,7 +33,6 @@ class CheckoutPaymentForm(forms.Form):
                                                                     'maxlength': 5,
                                                                     'title': 'mm/yy',
                                                                     'placeholder': 'mm/yy'}))
-
 
 
 
